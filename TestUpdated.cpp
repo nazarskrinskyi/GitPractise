181b6b007231ac710245658c541eaf98456b0dd9
Nazar <nazarharosh@gmail.com>
2023-04-06 19:36:06 +0300

    git --amend

--- TestUpdated.cpp
+++ TestUpdated.cpp
@@ -1,9 +1,10 @@
 //============================================================================
 // Name        : TestProg.cpp
 // Author      : Nazar
-// Version     :
+// Version     :qwewqerqwr
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
+//C:\Users\Asus\eclipse-workspace\TestProg\src
 //============================================================================
 
 #include <iostream>
@@ -13,22 +14,22 @@ using namespace std;
 int main()
 {
 	float x, c;
-	float f;
+	float f_;
 	cout << "value for x and c = ";
 	cin >> x >> c;
 	if (x > 0.5)
 	{
-		f = tan(sqrt(x) + x);
-		cout << "\nf = " << f << " x > 0.5";
+		f_ = tan(sqrt(x) + x);
+		cout << "\nf = " << f_ << " x > 0.5";
 	}
 	else if(0.1 <= x and x <= 0.5)
 	{
-		f = sin(abs(x - c));
-		cout << "\nf = " << f << " 0.1 <= x and x <= 0.5";
+		f_ = sin(abs(x - c));
+		cout << "\nf = " << f_ << " 0.1 <= x and x <= 0.5";
 	}
 	else
 	{
-		f = sqrt(abs(x + pow(cos(x-1), 2)));
-		cout << "\nf = " << f << " x < 0.1";
+		f_ = sqrt(abs(x + pow(cos(x-1), 2)));
+		cout << "\nf = " << f_ << " x < 0.1";
 	}
 }
